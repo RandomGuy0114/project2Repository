ec277910617db5d85da0a9c3bdca68f3db4378c4
PJAngeles023 <angeles.patrickjerowin@yahoo.com>
2022-12-18 20:01:03 +0800

    Firebase Auth User Working

--- Registration.js
+++ Registration.js
@@ -1,49 +1,51 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCp7jA3DrW9RCi5YFMj_fiJ-N8t-IF5M2E",
-    authDomain: "wd29-a8133.firebaseapp.com",
-    projectId: "wd29-a8133",
-    storageBucket: "wd29-a8133.appspot.com",
-    messagingSenderId: "600682734449",
-    appId: "1:600682734449:web:9a25f7ae7ae1688a5accd0",
-    measurementId: "G-SB9ZFSFS82"
-  };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCp7jA3DrW9RCi5YFMj_fiJ-N8t-IF5M2E",
+  authDomain: "wd29-a8133.firebaseapp.com",
+  databaseURL: "https://wd29-a8133-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wd29-a8133",
+  storageBucket: "wd29-a8133.appspot.com",
+  messagingSenderId: "600682734449",
+  appId: "1:600682734449:web:9a25f7ae7ae1688a5accd0",
+  measurementId: "G-SB9ZFSFS82"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 
   /*---------------------------------------------------------------- */
 
-  const registrationForm = document.getElementById("registrationForm");
+  const signupForm = document.getElementById("signupForm");
 
-  registrationForm.addEventListener("click", () => {
+
+  signupForm.addEventListener("click", () => {
+    const firstName = document.getElementById("userfirstName").value;
+    const lastName = document.getElementById("userlastName").value;
     const email = document.getElementById("userEmail").value;
     const password = document.getElementById("userPassword").value;
     const confirmPassword = document.getElementById("confirmPassword").value;
-    const displayName = document.getElementById("displayName").value;
 
     if(password === confirmPassword) {
       const auth = getAuth();
-      createUserWithEmailAndPassword(auth, email, password)
-        .then(() => {
-          updateProfile(auth.currentUser, {
-            displayName: displayName
-          });
-          window.location.replace("index.html");
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          // ..
-          console.log(error);
-          alert("There seems to be a problem with our server.")
-        });
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
     }else{
       alert("Passwords do not match.")
     }
