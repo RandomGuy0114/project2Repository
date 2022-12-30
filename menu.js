065f579ce19d6ba1dc01637af120c96dfb9c22ec
PJAngeles023 <angeles.patrickjerowin@yahoo.com>
2022-12-30 23:29:37 +0800

    Test

--- menu.js
+++ menu.js
@@ -1,3 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js'
+const firebaseConfig = {
+  apiKey: "AIzaSyCp7jA3DrW9RCi5YFMj_fiJ-N8t-IF5M2E",
+  authDomain: "wd29-a8133.firebaseapp.com",
+  databaseURL: "https://wd29-a8133-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wd29-a8133",
+  storageBucket: "wd29-a8133.appspot.com",
+  messagingSenderId: "600682734449",
+  appId: "1:600682734449:web:9a25f7ae7ae1688a5accd0",
+  measurementId: "G-SB9ZFSFS82"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getfirestore();
+
+
 document.getElementById('addtocart').addEventListener('click', function() {
 
     let name = document.getElementById('orderticket').value;
