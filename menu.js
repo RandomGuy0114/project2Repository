42c7903ee94a6dfbe23d5b62d99f7d21ddf57a08
PJAngeles023 <angeles.patrickjerowin@yahoo.com>
2023-01-01 20:58:44 +0800

    Last commit today

--- menu.js
+++ menu.js
@@ -1,64 +1,38 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js'
 const firebaseConfig = {
-  apiKey: "AIzaSyCp7jA3DrW9RCi5YFMj_fiJ-N8t-IF5M2E",
-  authDomain: "wd29-a8133.firebaseapp.com",
-  databaseURL: "https://wd29-a8133-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "wd29-a8133",
-  storageBucket: "wd29-a8133.appspot.com",
-  messagingSenderId: "600682734449",
-  appId: "1:600682734449:web:9a25f7ae7ae1688a5accd0",
-  measurementId: "G-SB9ZFSFS82"
+    apiKey: "AIzaSyCp7jA3DrW9RCi5YFMj_fiJ-N8t-IF5M2E",
+    authDomain: "wd29-a8133.firebaseapp.com",
+    databaseURL: "https://wd29-a8133-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "wd29-a8133",
+    storageBucket: "wd29-a8133.appspot.com",
+    messagingSenderId: "600682734449",
+    appId: "1:600682734449:web:9a25f7ae7ae1688a5accd0",
+    measurementId: "G-SB9ZFSFS82"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getfirestore();
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
 
+var addToCartButton = document.getElementById("add-to-cart-button");
+addToCartButton.addEventListener("click", function () {
+    db.collection("Food Menu").doc("Takoyaki").get().then(function (doc) {
+        if (doc.exists) {
+            var data = doc.data();
+            var array = data.array;
+            updateModal(array[0], array[1], array[2]);
+        }
+    });
 
-document.getElementById('addtocart').addEventListener('click', function() {
+    db.collection("TakoyakiA").get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+            var data = doc.data();
+            var array = data.array;
+            updateModal(array[0], array[1], array[2]);
+        });
+    });
+});
 
-    let name = document.getElementById('orderticket').value;
-    let quantity = document.getElementById('quantity').value;
-    let price = document.getElementById('price').value;
-    let totalPrice = document.getElementById('total').value;
-
-    
-    addToCart(name, quantity, price);
-  });
-  
-  function addToCart(name, price, totalPrice) {
-    // Add the item to the shopping cart
-  const order = {
-    name: name,
-    price: price,
-    quantity: 1,
-    total: totalPrice
-  };
-    foodCart.push(order);
-
-  // Update the cart total
-  updateCartTotal();
-  }
-
-  function updateCartTotal() {
-    let totalCost = 0;
-    for (const order of foodCart) {
-      totalCost += order.price * order.quantity;
-    }
-    document.querySelector('#pricetotal').textContent = `Total: $${totalCost}`;
-  }
-
-  function displayTicket(name, quantity, price) {
-    let ticketDisplay = document.getElementById('ticketdisplay');
-    ticketDisplay.innerHTML = ''; // pang refresh or clear
-    
-    // Generate the ticket display HTML
-    let ticket = '<h2>Your Ticket</h2>';
-    ticket += '<p>Order: ' + name + '</p>';
-    ticket += '<p>Quantity: ' + quantity + '</p>';
-    ticket += '<p>Price: ' + price + '</p>';
-    
-    // Add the ticket display HTML to the ticket display element
-    ticketDisplay.innerHTML = ticket;
-  }
+function updateModal(name, price, quantity) {
+    // Update the modal with the given data, eto lead lagay ka id sa modal tapos tawagin mo dito
+}
