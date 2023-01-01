1b90f23e0c43e0b6160f76221b4ba72a25b6c0fa
PJAngeles023 <angeles.patrickjerowin@yahoo.com>
2023-01-01 18:36:26 +0800

    test

--- menu.js
+++ menu.js
@@ -1,36 +1,39 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCp7jA3DrW9RCi5YFMj_fiJ-N8t-IF5M2E",
-  authDomain: "wd29-a8133.firebaseapp.com",
-  databaseURL: "https://wd29-a8133-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "wd29-a8133",
-  storageBucket: "wd29-a8133.appspot.com",
-  messagingSenderId: "600682734449",
-  appId: "1:600682734449:web:9a25f7ae7ae1688a5accd0",
-  measurementId: "G-SB9ZFSFS82"
-};
+document.getElementById('addtocart').addEventListener('click', function() {
 
-firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore();
+    let orderTicket = document.getElementById('orderticket').value;
+    let quantity = document.getElementById('quantity').value;
+    let price = document.getElementById('price').value;
+    let totalPrice = document.getElementById('total').value;
 
-var addToCartButton = document.getElementById("add-to-cart-button");
-addToCartButton.addEventListener("click", function() {
-  db.collection("Food Menu").doc("Takoyaki").get().then(function(doc) {
-    if (doc.exists) {
-      var data = doc.data();
-      var array = data.array;
-      updateModal(array[0], array[1], array[2]);
-    }
+    
+    addToCart(orderTicket, quantity, price, totalPrice);
   });
-
-  db.collection("TakoyakiA").get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-      var data = doc.data();
-      var array = data.array;
-      updateModal(array[0], array[1], array[2]);
-    });
-  });
-});
-
-function updateModal(name, price, quantity) {
   
-}
+  function addToCart(orderTicket, price, totalPrice) {
+    // Add the item to the shopping cart
+  const item = {
+    order: orderTicket,
+    price: price,
+    quantity: 1,
+    total: totalPrice
+  };
+    foodCart.push(item);
+
+  // Update the cart total
+  updateCartTotal();
+  }
+
+  function displayTicket(orderTicket, quantity, price, totalPrice) {
+    let ticketDisplay = document.getElementById('ticketdisplay');
+    ticketDisplay.innerHTML = ''; // pang refresh or clear
+    
+    // Generate the ticket display HTML
+    let ticket = '<h2>Your Ticket</h2>';
+    ticket += '<p>Order Ticket: ' + orderTicket + '</p>';
+    ticket += '<p>Quantity: ' + quantity + '</p>';
+    ticket += '<p>Price: ' + price + '</p>';
+    ticket += '<p>Total: ' + totalPrice + '</p>';
+    
+    // Add the ticket display HTML to the ticket display element
+    ticketDisplay.innerHTML = ticket;
+  }
