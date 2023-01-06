364a2c437c45739ac01bf025890ea18e5254ba4d
codeace110 <neri11032321@gmail.com>
2023-01-07 01:17:44 +0800

    added public directory

---
+++ public/js/login.js
@@ -0,0 +1,13 @@
+let globalEmail = "";
+
+async function login() {
+  console.log('Attempting to login user ...')
+  let userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+  try {
+    const response = await firebase.auth().signInWithEmailAndPassword(userEmail, userPass);
+    globalEmail = response.user.email;
+  } catch (error) {
+    errorNotification(error.message);
+  }
+}
