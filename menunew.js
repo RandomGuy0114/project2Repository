5c096b094673512352b5f32bf293c85d1fb32d67
RandomGuy0114 <clarenceperez686@gmail.com>
2023-01-11 20:55:35 +0800

    asdads

--- menunew.js
+++ menunew.js
@@ -1,7 +1,7 @@
-
+let totalOfAll = 0;
 window.onload = function () {
 
-  let totalLast=0;
+
   // for customer choices of quantity.
   const buttons = document.querySelectorAll('.add, .subtract');
 
@@ -39,8 +39,8 @@ window.onload = function () {
 
         document.getElementById("mySidenav").style.width = "250px";
 
-        
-        
+
+
         // Get the value of the data-value attribute
         const container = event.target.parentNode.parentNode;
 
@@ -50,67 +50,64 @@ window.onload = function () {
         const div = container.querySelector('.middle');
         const foodName = div.dataset.food;
 
-
         const img = container.querySelector('.image');
         const foodImage = img.getAttribute('src');
 
-        const totalPrice = value * input.value;
+        const totalPrice = parseInt(value) * input.value;
+        totalOfAll += totalPrice;
         const modal = document.querySelector('#modal');
 
         modal.innerHTML += `
         <div id="modalOrig" class="col-12 pl-2 pr-4 mb-3">
 
-      <img class="mr-3" src=${foodImage} style="width: 50px; height: 50px; float:left; border-radius:500%;">
-      <button class="fa-solid fa-trash" style="border:none; background-color:white; float:right"></button>
-      <h4 style="font-size:medium;">${foodName}</h4>
-      <h6 style="float:left;">Qty: ${input.value}</h6>
-
-      <p style="float:right; margin-bottom:0rem; padding-top:0rem;">Price: ${value}</p><br>
-      <p id="total" style="float:right; margin-bottom:0rem;  padding-top:0rem;">Total Price: ${totalPrice}</p><br>
-
-      <br>
-      </div>
-      `;
-        // getting all of the total of orders.
-        modal.querySelector('#total').setAttribute('data-totall', totalPrice)
-        const totalSaIsangItem = modal.querySelector('#total').dataset.totall;
-        console.log(totalSaIsangItem);
-        const sideNavTotal = document.querySelector('#mySidenav').childNodes[7].childNodes[1];
-        console.log(sideNavTotal);
-        // const modalTotal = sumTotalPrices()
-        // 
+        <img class="mr-3" src=${foodImage} style="width: 50px; height: 50px; float:left; border-radius:500%;">
+        <button class="fa-solid fa-trash" style="border:none; background-color:white; float:right"></button>
+        <h4 style="font-size:medium;">${foodName}</h4>
+        <h6 style="float:left;">Qty: ${input.value}</h6>
+      
+        <p style="float:right; margin-bottom:0rem; padding-top:0rem;">Price: ${value}</p><br>
+        <p id="total" style="float:right; margin-bottom:0rem; padding-top:0rem;" data-totall="${totalPrice}">Total Price: ${totalPrice}</p><br>
+      
+        <br>
+        </div>
+        `;
+        console.log('nadagdagan ',totalOfAll, typeof totalOfAll);
         
-        const totalValues = modal.querySelectorAll('#total');
-        totalValues.forEach((totalvalue)=>{
-          totalLast += parseInt(totalvalue.dataset.totall);
-          totalLast += isNaN(value) ? 0 : value;
-        })
-        ;
-        sideNavTotal.innerHTML = `Total: ${totalLast}`
-        // kada pindut sa addToCart yung input quantity maging 0.
+        // Get the side navigation menu's total element
+        const sideNavTotal = document.querySelector('#mySidenav').childNodes[7].childNodes[1];
+        sideNavTotal.innerHTML = `Total:  ${totalOfAll}`;
+
+        // modal.querySelector('#total').setAttribute('data-totall', totalPrice)
+
         input.value = 0;
 
         modal.style.display = "block";
-
-
+        
+        const totalOneItem = document.querySelector('#modalOrig').querySelector('#total').getAttribute('data-totall');
+        
         // Get a list of all the trash button elements
         const trashButtons = modal.querySelectorAll('.fa-trash');
-        // Add a click event listener to each trash button
         trashButtons.forEach(function (trashButton) {
           trashButton.addEventListener('click', function () {
-            // Remove the parent element of the trash button (which is the item)
-            this.parentElement.remove();
+            const parent = trashButton.parentNode;
+            const totalOneItem = parent.querySelector('#total').getAttribute('data-totall');
+            const totalOneItemInt = parseInt(totalOneItem);
+            console.log('')
+            console.log('bawas: ',totalOneItem,parseInt(totalOneItem));
+            console.log('totalOfAll: ',totalOfAll,typeof totalOfAll)
+            console.log('totalOneItem: ',totalOneItem,typeof totalOneItem)
+            console.log('totalOneItemInt: ',totalOneItemInt,typeof totalOneItemInt)
+            console.log('')
+            totalOfAll -= totalOneItemInt;
+            console.log('totalOfAll: ',totalOfAll,typeof totalOfAll);
+            sideNavTotal.innerHTML = `Total:  ${totalOfAll}`;
+            // const sibling = trashButton.nextElementSibling.nextElementSibling.nextElementSibling.getAttribute('id');
+            // console.log(sibling);
+            // console.log(parent.innerHTML);
+            parent.remove();
+
           });
         });
-
-        // function sumTotalPrices() {
-        //   const modalOrigElements = modal.querySelectorAll('[data-totall]');
-        //   const total = 0;
-        //   modalOrigElements.forEach(element => {
-        //     total += parseInt(element.dataset.totall);
-        //   });
-        //   return total;
-        // }
 
 
       }
@@ -120,11 +117,26 @@ window.onload = function () {
   });
 
 
-  // updating the total in modal.
-  
-  
-  
 
+
+
+
+  // getting all of the total of orders.
+  // modal.querySelector('#total').setAttribute('data-totall', totalPrice)
+  // const totalSaIsangItem = modal.querySelector('#total').dataset.totall;
+  // console.log(totalSaIsangItem);
+  // const sideNavTotal = document.querySelector('#mySidenav').childNodes[7].childNodes[1];
+  // console.log(sideNavTotal);
+
+
+
+  // const totalValues = modal.querySelectorAll('#total');
+  // totalValues.forEach((totalvalue)=>{
+  //   totalLast += parseInt(totalvalue.dataset.totall);
+  //   totalLast += isNaN(value) ? 0 : value;
+  // });
+  // sideNavTotal.innerHTML = `Total: ${totalLast}`
+  // kada pindut sa addToCart yung input quantity maging 0.
 
 
 
