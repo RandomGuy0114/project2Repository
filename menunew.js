a4410676603d7a8e851b0e3de07fd52b5b47b921
RandomGuy0114 <clarenceperez686@gmail.com>
2023-01-11 22:19:58 +0800

    dasdas

--- menunew.js
+++ menunew.js
@@ -52,26 +52,82 @@ window.onload = function () {
 
         const img = container.querySelector('.image');
         const foodImage = img.getAttribute('src');
-
+        let uid = "";
+        // making an id for div.
+        switch (foodName) {
+          case 'Sushi Boat':
+            uid = 'sushiboatz';
+            break;
+          case 'Tan Tan':
+            uid = 'tantanz';
+            break;
+          case 'Nikutama':
+            uid = 'nikutamaz';
+            break;
+          case 'Tonkotsu':
+            uid = 'tonkotsuz';
+            break;
+          case 'Original':
+            uid = 'originalz';
+            break;
+          case 'Jalapeño':
+            uid = 'jalapeñoz';
+            break;
+          case 'Wasabi':
+            uid = 'wasabiz';
+            break;
+          case 'Mentaiko':
+            uid = 'mentaikoz';
+            break;
+          case 'Salmon Sushi Roll':
+            uid = 'salmonsushirollz';
+            break;
+          case 'Smoked Salmon Sushi':
+            uid = 'smokedsalmonsushiz';
+            break;
+          case 'Palaeo Sushi':
+            uid = 'palaeosushiz';
+            break;
+        }
         const totalPrice = parseInt(value) * input.value;
         totalOfAll += totalPrice;
         const modal = document.querySelector('#modal');
 
-        modal.innerHTML += `
-        <div id="modalOrig" class="col-12 pl-2 pr-4 mb-3">
+        if (modal.querySelector(`#${uid}`)){
+          // updating the quantity if the order is already exist.
+          const kuwantiti = modal.querySelector(`#${uid}`).querySelector('#kuwantiti');
+          const kwt = kuwantiti.getAttribute('data-qnt')
+          const kwtt = parseInt(input.value) + parseInt(kwt);
+          kuwantiti.setAttribute('data-qnt',kwtt);
+          kuwantiti.innerHTML = `Qty: ${kwtt}`;
+          // console.log(dtotal,kwtt);
+
+          const ptotal = modal.querySelector(`#${uid}`).querySelector('#total');
+          const dtotal = ptotal.getAttribute('data-totall');
+          console.log(parseInt(dtotal));
+          const ddtotal = parseInt(dtotal) + parseInt(totalPrice);
+          console.log(ddtotal);
+          ptotal.setAttribute('data-totall',ddtotal);
+          ptotal.innerHTML =`Total Price: ${ddtotal}`
+
+        }else{
+          modal.innerHTML += `
+        <div id="${uid}" class="col-12 pl-2 pr-4 mb-3">
 
         <img class="mr-3" src=${foodImage} style="width: 50px; height: 50px; float:left; border-radius:500%;">
         <button class="fa-solid fa-trash" style="border:none; background-color:white; float:right"></button>
         <h4 style="font-size:medium;">${foodName}</h4>
-        <h6 style="float:left;">Qty: ${input.value}</h6>
+        <h6 id="kuwantiti" style="float:left;" data-qnt="${input.value}">Qty: ${input.value}</h6>
         <p style="float:right; margin-bottom:0rem; padding-top:0rem;">Price: ${value}</p><br>
         <p id="total" style="float:right; margin-bottom:0rem; padding-top:0rem;" data-totall="${totalPrice}">Total Price: ${totalPrice}</p><br>
       
         <br>
         </div>
-        `;
-        console.log('nadagdagan ',totalOfAll, typeof totalOfAll);
+        `;}
         
+        
+        console.log('nadagdagan ', totalOfAll, typeof totalOfAll);
+
         // Get the side navigation menu's total element
         const sideNavTotal = document.querySelector('#mySidenav').childNodes[7].childNodes[1];
         sideNavTotal.innerHTML = `Total:  ${totalOfAll}`;
@@ -81,23 +137,22 @@ window.onload = function () {
         input.value = 0;
 
         modal.style.display = "block";
-        
-        const totalOneItem = document.querySelector('#modalOrig').querySelector('#total').getAttribute('data-totall');
-        
+
+
+
         // Get a list of all the trash button elements
         const trashButtons = modal.querySelectorAll('.fa-trash');
         trashButtons.forEach(function (trashButton) {
           trashButton.addEventListener('click', function () {
             const parent = trashButton.parentNode;
+
+            // updating the total in sidebar when user delete an order.
             const totalOneItem = parent.querySelector('#total').getAttribute('data-totall');
             const totalOneItemInt = parseInt(totalOneItem);
-            
+
             totalOfAll -= totalOneItemInt;
-            console.log('totalOfAll: ',totalOfAll,typeof totalOfAll);
+            console.log('totalOfAll: ', totalOfAll, typeof totalOfAll);
             sideNavTotal.innerHTML = `Total:  ${totalOfAll}`;
-            // const sibling = trashButton.nextElementSibling.nextElementSibling.nextElementSibling.getAttribute('id');
-            // console.log(sibling);
-            // console.log(parent.innerHTML);
             parent.remove();
 
           });
