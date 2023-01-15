f9f3bd2a2a0096623425938fbe9ef88076a10885
PJAngeles023 <angeles.patrickjerowin@yahoo.com>
2023-01-16 02:03:57 +0800

    City, Barangay, Landmark, Contact Number valid

--- modalForm.js
+++ modalForm.js
@@ -17,51 +17,76 @@ let db = firebase.firestore();
 let proceedBtn = document.getElementById("proceed-button");
 
 // Add an event listener to the button
-proceedBtn.addEventListener("click", function (event) {
+proceedBtn.addEventListener("click", function () {
   let isNumber = /^\d+$/;
+  let isLetters = /^[A-Za-z]+$/;
+
+  // Get the data from the form inputs
+  let houseno = document.getElementById("houseno").value;
+  let street = document.getElementById("street").value;
+  let barangay = document.getElementById("barangay").value;
+  let city = document.getElementById("city").value;
+  let landmark = document.getElementById("landmark").value;
+  let contact = document.getElementById("contact").value;
+  let specialRequest = document.getElementById("specialRequest").value;
+  let totalInput = document.getElementById("totalDiv").innerText;
+  let user = document.getElementById("user_Checkout").innerText;
 
   // check if may laman ung contact
-  if (!contact) {
-    alert("Contact number is empty. Please enter a valid 11-digit mobile number starting with 09.");
-    return;
+  if (!contact || !houseno || !street || !barangay || !city || !landmark || !specialRequest) {
+    $('.alert-danger').show();
+    setTimeout(function () {
+      $('.alert-danger').hide();
+    }, 3000);
   }
   else {
-    // Get the data from the form inputs
-    let houseno = document.getElementById("houseno").value;
-    let street = document.getElementById("street").value;
-    let barangay = document.getElementById("barangay").value;
-    let city = document.getElementById("city").value;
-    let landmark = document.getElementById("landmark").value;
-    let contact = document.getElementById("contact").value;
-    let specialRequest = document.getElementById("specialRequest").value;
-    let totalInput = document.getElementById("totalDiv").innerText;
-    let user = document.getElementById("user_Checkout").innerText;
 
+    let barangay = document.getElementById("barangay").value;
+    if (!isLetters.test(barangay)) {
+      alert("Barangay: use letters only");
+      return;
+    }
+    let city = document.getElementById("city").value;
+    if (!isLetters.test(city)) {
+      alert("City: use letters only");
+      return;
+    }   
+    let landmark = document.getElementById("landmark").value;
+    if (!isLetters.test(landmark)) {
+      alert("Landmark: use letters only");
+      return;
+    }
     //check kung ang first two letters ay 09 at 11 characters
     if (!isNumber.test(contact) || !contact.startsWith("09") || contact.length !== 11) {
       alert("Invalid contact number. Please enter a valid 11-digit mobile number starting with 09.");
-      event.preventDefault();
       return;
     }
-    // Add the data to the "Orders" collection
-    db.collection("Orders").add({
-      houseno: houseno,
-      street: street,
-      barangay: barangay,
-      city: city,
-      landmark: landmark,
-      contact: contact,
-      specialRequest: specialRequest,
-      totalInput: totalInput,
-      user: user,
-    })
-      .then(function (docRef) {
-        console.log("Order added with ID: ", docRef.id);
-          redirect();
+    else {
+      // Add the data to the "Orders" collection
+      db.collection("Orders").add({
+        houseno: houseno,
+        street: street,
+        barangay: barangay,
+        city: city,
+        landmark: landmark,
+        contact: contact,
+        specialRequest: specialRequest,
+        totalInput: totalInput,
+        user: user,
+      })
+        .then(function (docRef) {
+          console.log("Order added with ID: ", docRef.id);
+          $("#exampleModalToggle2").modal("show");
+          setTimeout(function () {
+            $("#exampleModalToggle2").hide();
+            redirect();
+          }, 3000);
         })
-      .catch(function (error) {
-        console.error("Error adding Order: ", error);
-      });
+        .catch(function (error) {
+          console.error("Error adding Order: ", error);
+          $("#exampleModalToggle2").modal("hide");
+        });
+    }
   }
 });
 
--- userCheckOut.js
+++ userCheckOut.js
@@ -1,25 +1,25 @@
-$(document).ready(function() {
-    $('#proceed-button').click(function() {
-      var isValid = true;
-      $('input').each(function() {
-        if ($(this).val() === "") {
-          isValid = false;
-          return false;
-        }
+// $(document).ready(function() {
+//     $('#proceed-button').click(function() {
+//       var isValid = true;
+//       $('input').each(function() {
+//         if ($(this).val() === "") {
+//           isValid = false;
+//           return false;
+//         }
         
-      });
-      if (isValid === true) {
-        $("#exampleModalToggle2").modal("show");
-        $("#exampleModalToggle").modal("hide");
+//       });
+//       if (isValid === true) {
+//         $("#exampleModalToggle2").modal("show");
+//         $("#exampleModalToggle").modal("hide");
         
-        } else {
-           $('.alert-danger').show();
-          setTimeout(function(){
-            $('.alert-danger').hide();
-          }, 3000);
-        }
-    });
-  });
+//         } else {
+//            $('.alert-danger').show();
+//           setTimeout(function(){
+//             $('.alert-danger').hide();
+//           }, 3000);
+//         }
+//     });
+//   });
   
 
 //   function modalTrigger(){
