1e4c0c2184d94ec61d59743d9b3d0106749a64da
codeace110 <neri11032321@gmail.com>
2023-01-17 17:07:26 +0800

    merge login

--- modalForm.js
+++ modalForm.js
@@ -1,16 +1,3 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCp7jA3DrW9RCi5YFMj_fiJ-N8t-IF5M2E",
-  authDomain: "wd29-a8133.firebaseapp.com",
-  databaseURL:
-    "https://wd29-a8133-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "wd29-a8133",
-  storageBucket: "wd29-a8133.appspot.com",
-  messagingSenderId: "600682734449",
-  appId: "1:600682734449:web:9a25f7ae7ae1688a5accd0",
-  measurementId: "G-SB9ZFSFS82",
-};
-
-// Get a reference to the Firestore database
 firebase.initializeApp(firebaseConfig);
 let db = firebase.firestore();
 let ordersRef = db.collection("Orders").doc("orderId");
