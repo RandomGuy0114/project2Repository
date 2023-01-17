d3301876e7295b6730c3e682cbd15aba21e730da
codeace110 <neri11032321@gmail.com>
2023-01-17 14:43:51 +0800

    merge login

--- modalForm.js
+++ modalForm.js
@@ -19,15 +19,22 @@ let orderTicketRef = ordersRef.collection("OrderTicket");
 
 // Get a reference to the "Proceed" button
 let proceedBtn = document.getElementById("proceed-button");
-
-// Add an event listener to the button
-proceedBtn.addEventListener("click", function () {
+// checking if user is signed in
+firebase.auth().onAuthStateChanged(function(user){
+  if (user) {
+    // User is signed in and verified
+    let userName = user.displayName;
+    let userEmail = user.email;
+    let userId = user.uid;
+    console.log("User is signed in and verified: ", userName, userEmail, userId);
+    // Add the user's name, email, and ID to the order data
+    // before adding it to the "Orders" collection
   
-  let isNumber = /^\d+$/;
-  let isLetters = /^[A-Za-z]+$/;
-  let isOneSpace = /^[A-Za-z]+\s[A-Za-z]+$/;
-
-  // Get the data from the form inputs
+    proceedBtn.addEventListener("click"), function () {
+    let isNumber = /^\d+$/;
+    let isLetters = /^[A-Za-z]+$/;
+    let isOneSpace = /^[A-Za-z]+\s[A-Za-z]+$/;
+      // Get the data from the form inputs
   let houseno = document.getElementById("houseno").value;
   let street = document.getElementById("street").value;
   let barangay = document.getElementById("barangay").value;
@@ -40,7 +47,7 @@ proceedBtn.addEventListener("click", function () {
   let specialRequest = document.getElementById("specialRequest").value;
   let totalInput = document.getElementById("totalDiv").innerText;
   let order = document.getElementById("user_Checkout").innerText;
-
+  
   // check if may laman ung contact
   if (
     !contact ||
@@ -85,43 +92,189 @@ proceedBtn.addEventListener("click", function () {
       contact.length !== 11
     ) {
       alert(
-        "Invalid contact number. Please enter a valid 11-digit mobile number starting with 09."
-      );
-      return;
-    } else {
-      // Add the data to the "Orders" collection
+        "Invalid contact number. Please enter a valid 11-digit mobile number starting with 09.");
+        return;
+        } else {
+        // Check if the user is signed in
+        firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+        // User is signed in and verified
+        let userName = user.displayName;
+        let userEmail = user.email;
+        let userId = user.uid;
+              // Add the user's name, email, and ID to the order data 
+      // before adding it to the "Orders" collection
       orderTicketRef
-        .add({
-          HouseNo: houseno,
-          Street: street,
-          Barangay: barangay,
-          City: city,
-          Landmark: landmark,
-          Contact: contact,
-          Payment: paymentMethod,
-          SpecialRequest: specialRequest,
-          TotalPrice: totalInput,
-          Order: order,
-        })
-        .then(function (docRef) {
-          console.log("Order added with ID: ", docRef.id);
-          let orderNo = "Order No. " + docRef.id;
-          console.log(orderNo);
-          $("#exampleModalToggle2").modal("show");
-          setTimeout(function () {
-            $("#exampleModalToggle2").hide();
-            redirect();
-          }, 3000);
-        })
-        .catch(function (error) {
-          console.error("Error adding Order: ", error);
-          $("#exampleModalToggle2").modal("hide");
-        });
-    }
+      .add({
+        HouseNo: houseno,
+        Street: street,
+        Barangay: barangay,
+        City: city,
+        Landmark: landmark,
+        Contact: contact,
+        Payment: paymentMethod,
+        SpecialRequest: specialRequest,
+        TotalPrice: totalInput,
+        Order: order,
+        UserName: userName,
+        UserEmail: userEmail,
+        UserId: userId,
+      })
+      .then(function (docRef) {
+        console.log("Order added with ID: ", docRef.id);
+        let orderNo = "Order No. " + docRef.id;
+        window.location.href = "success.html?orderNo=" + orderNo;
+      })
+      .catch(function (error) {
+        console.error("Error adding order: ", error);
+      });
+  } else {
+    // User is not signed in
+    errorNotification("Please login to Continue!");
   }
 });
-
-function redirect() {
-  window.location.href = "userDashboard.html";
 }
+}
+};
 
+// Function to display error notification
+function errorNotification(message) 
+{
+// Display the error message
+alert(message);
+}}})
+
+
+// // Get a reference to the Firestore database
+// firebase.initializeApp(firebaseConfig);
+// let db = firebase.firestore();
+// let ordersRef = db.collection("Orders").doc("orderId");
+// let orderTicketRef = ordersRef.collection("OrderTicket");
+// // let idUser = db.collection("userData").user.uid("userID")
+
+// // Get a reference to the "Proceed" button
+// let proceedBtn = document.getElementById("proceed-button");
+// // checking if user is signed in
+// firebase.auth().onAuthStateChanged(function(user){
+//   if (user) {
+//     // User is signed in and verified
+//     let userName = user.displayName;
+//     let userEmail = user.email;
+//     let userId = user.uid;
+//     console.log("User is signed in and verified: ", userName, userEmail, userId);
+//     // Add the user's name, email, and ID to the order data
+//     // before adding it to the "Orders" collection
+  
+//     proceedBtn.addEventListener("click"), function () {
+//     let isNumber = /^\d+$/;
+//     let isLetters = /^[A-Za-z]+$/;
+//     let isOneSpace = /^[A-Za-z]+\s[A-Za-z]+$/;
+//       // Get the data from the form inputs
+//   let houseno = document.getElementById("houseno").value;
+//   let street = document.getElementById("street").value;
+//   let barangay = document.getElementById("barangay").value;
+//   let city = document.getElementById("city").value;
+//   let landmark = document.getElementById("landmark").value;
+//   let contact = document.getElementById("contact").value;
+//   let paymentMethod = document.querySelector(
+//     'input[name="flexRadioDefault"]:checked'
+//   ).value;
+//   let specialRequest = document.getElementById("specialRequest").value;
+//   let totalInput = document.getElementById("totalDiv").innerText;
+//   let order = document.getElementById("user_Checkout").innerText;
+  
+//   // check if may laman ung contact
+//   if (
+//     !contact ||
+//     !houseno ||
+//     !street ||
+//     !barangay ||
+//     !city ||
+//     !landmark ||
+//     !specialRequest ||
+//     !paymentMethod
+//   ) {
+//     $(".alert-danger").show();
+//     setTimeout(function () {
+//       $(".alert-danger").hide();
+//     }, 3000);
+//   } else {
+//     let barangay = document.getElementById("barangay").value;
+//     if (!isLetters.test(barangay) && !isOneSpace.test(barangay)) {
+//       alert("Barangay: use letters and 1 space only");
+//       return;
+//     }
+//     let city = document.getElementById("city").value;
+//     if (!isLetters.test(city) && !isOneSpace.test(city)) {
+//       alert("City: use letters and 1 space only");
+//       return;
+//     }
+//     let landmark = document.getElementById("landmark").value;
+//     if (!isLetters.test(landmark) && !isOneSpace.test(landmark)) {
+//       alert("Landmark: use letters and 1 space only");
+//       return;
+//     }
+
+//     let street = document.getElementById("street").value;
+//     if (!isLetters.test(street) && !isOneSpace.test(street)) {
+//       alert("Street: use letters and 1 space only");
+//       return;
+//     }
+//     //check kung ang first two letters ay 09 at 11 characters
+//     if (
+//       !isNumber.test(contact) ||
+//       !contact.startsWith("09") ||
+//       contact.length !== 11
+//     ) {
+//       alert(
+//         "Invalid contact number. Please enter a valid 11-digit mobile number starting with 09.");
+//         return;
+//         } else {
+//         // Check if the user is signed in
+//         firebase.auth().onAuthStateChanged(function (user) {
+//         if (user) {
+//         // User is signed in and verified
+//         let userName = user.displayName;
+//         let userEmail = user.email;
+//         let userId = user.uid;
+//               // Add the user's name, email, and ID to the order data 
+//       // before adding it to the "Orders" collection
+//       orderTicketRef
+//       .add({
+//         HouseNo: houseno,
+//         Street: street,
+//         Barangay: barangay,
+//         City: city,
+//         Landmark: landmark,
+//         Contact: contact,
+//         Payment: paymentMethod,
+//         SpecialRequest: specialRequest,
+//         TotalPrice: totalInput,
+//         Order: order,
+//         UserName: userName,
+//         UserEmail: userEmail,
+//         UserId: userId,
+//       })
+//       .then(function (docRef) {
+//         console.log("Order added with ID: ", docRef.id);
+//         let orderNo = "Order No. " + docRef.id;
+//         window.location.href = "success.html?orderNo=" + orderNo;
+//       })
+//       .catch(function (error) {
+//         console.error("Error adding order: ", error);
+//       });
+//   } else {
+//     // User is not signed in
+//     errorNotification("Please login to Continue!");
+//   }
+// });
+// }
+// }
+// };
+
+// // Function to display error notification
+// function errorNotification(message) 
+// {
+// // Display the error message
+// alert(message);
+// }}})
