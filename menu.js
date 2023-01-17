ce1959719b126176fe138e9aa1eaedf9eaf2f5dc
RandomGuy0114 <clarenceperez686@gmail.com>
2023-01-17 20:56:31 +0800

    editing the menu

--- menu.js
+++ menu.js
@@ -2,113 +2,128 @@ let totalOfAll = 0;
 
 
 
-  // for customer choices of quantity.
-  const buttons = document.querySelectorAll('.add, .subtract');
+// for customer choices of quantity.
+const buttons = document.querySelectorAll('.add, .subtract');
 
-  // Add a click event listener to each of the buttons
-  buttons.forEach(button => {
-    button.addEventListener('click', () => {
-      // Find the parent element of the button
-      const parent = button.parentNode;
-      // Find the input element within the parent element
-      const input = parent.querySelector('.inputQuantity');
-      // Increment or decrement the value of the input element
-      if (button.classList.contains('add')) {
-        input.value = parseInt(input.value) + 1;
-      } else if (button.classList.contains('subtract')) {
-        if (input.value > 0) {
-          input.value = parseInt(input.value) - 1;
-        }
+// Add a click event listener to each of the buttons
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Find the parent element of the button
+    const parent = button.parentNode;
+    // Find the input element within the parent element
+    const input = parent.querySelector('.inputQuantity');
+    // Increment or decrement the value of the input element
+    if (button.classList.contains('add')) {
+      input.value = parseInt(input.value) + 1;
+    } else if (button.classList.contains('subtract')) {
+      if (input.value > 0) {
+        input.value = parseInt(input.value) - 1;
       }
-    });
+    }
   });
+});
 
 
 
-  // functions for clicking the menu buttons.
+// functions for clicking the menu buttons.
 
-  document.addEventListener('click', (event) => {
-    if (event.target.classList.contains('addToCart')) {
-      // Find the parent element of the clicked button
-      const parent = event.target.parentNode;
-      // Find the input element within the parent element
-      const input = parent.querySelector('.inputQuantity');
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('addToCart')) {
+    // Find the parent element of the clicked button
+    const parent = event.target.parentNode;
+    // Find the input element within the parent element
+    const input = parent.querySelector('.inputQuantity');
 
-      // Reset the value of the input element to 0
-      if (input.value != 0) {
+    // Reset the value of the input element to 0
+    if (input.value != 0) {
 
-        document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("mySidenav").style.width = "250px";
 
-        // Get the value of the data-value attribute
-        const container = event.target.parentNode.parentNode;
+      // Get the value of the data-value attribute
+      const container = event.target.parentNode.parentNode;
 
-        const h5 = container.querySelector('.foodPrice');
-        const value = h5.dataset.value;
-        // const parent = event.target.parentNode;
-        const div = container.querySelector('.middle');
-        const foodName = div.dataset.food;
+      const h5 = container.querySelector('.foodPrice');
+      const value = h5.dataset.value;
+      // const parent = event.target.parentNode;
+      const div = container.querySelector('.middle');
+      const foodName = div.dataset.food;
 
-        const img = container.querySelector('.image');
-        const foodImage = img.getAttribute('src');
-        let uid = "";
-        // making an id for div.
-        switch (foodName) {
-          case 'Sushi Boat':
-            uid = 'sushiboatz';
-            break;
-          case 'Tan Tan':
-            uid = 'tantanz';
-            break;
-          case 'Nikutama':
-            uid = 'nikutamaz';
-            break;
-          case 'Tonkotsu':
-            uid = 'tonkotsuz';
-            break;
-          case 'Original':
-            uid = 'originalz';
-            break;
-          case 'Jalapeño':
-            uid = 'jalapeñoz';
-            break;
-          case 'Wasabi':
-            uid = 'wasabiz';
-            break;
-          case 'Mentaiko':
-            uid = 'mentaikoz';
-            break;
-          case 'Salmon Sushi Roll':
-            uid = 'salmonsushirollz';
-            break;
-          case 'Smoked Salmon Sushi':
-            uid = 'smokedsalmonsushiz';
-            break;
-          case 'Palaeo Sushi':
-            uid = 'palaeosushiz';
-            break;
-        }
-        const totalPrice = parseInt(value) * input.value;
-        totalOfAll += totalPrice;
-        const modal = document.querySelector('#modal');
+      const img = container.querySelector('.image');
+      const foodImage = img.getAttribute('src');
+      let uid = "";
+      // making an id for div.
+      switch (foodName) {
+        case 'Sushi Boat':
+          uid = 'sushiboatz';
+          break;
+        case 'Tan Tan':
+          uid = 'tantanz';
+          break;
+        case 'Nikutama':
+          uid = 'nikutamaz';
+          break;
+        case 'Tonkotsu':
+          uid = 'tonkotsuz';
+          break;
+        case 'Original':
+          uid = 'originalz';
+          break;
+        case 'Jalapeño':
+          uid = 'jalapeñoz';
+          break;
+        case 'Wasabi':
+          uid = 'wasabiz';
+          break;
+        case 'Mentaiko':
+          uid = 'mentaikoz';
+          break;
+        case 'Salmon Sushi Roll':
+          uid = 'salmonsushirollz';
+          break;
+        case 'Smoked Salmon Sushi':
+          uid = 'smokedsalmonsushiz';
+          break;
+        case 'Palaeo Sushi':
+          uid = 'palaeosushiz';
+          break;
+      }
+      const totalPrice = parseInt(value) * input.value;
+      totalOfAll += totalPrice;
+      const modal = document.querySelector('#modal');
 
-        if (modal.querySelector(`#${uid}`)) {
-          // updating the quantity if the order is already exist.
-          const kuwantiti = modal.querySelector(`#${uid}`).querySelector('#kuwantiti');
-          const kwt = kuwantiti.getAttribute('data-qnt')
-          const kwtt = parseInt(input.value) + parseInt(kwt);
-          kuwantiti.setAttribute('data-qnt', kwtt);
-          kuwantiti.innerHTML = `Qty: ${kwtt}`;
+      if (modal.querySelector(`#${uid}`)) {
+        // updating the quantity if the order is already exist.
+        const kuwantiti = modal.querySelector(`#${uid}`).querySelector('#kuwantiti');
+        const kwt = kuwantiti.getAttribute('data-qnt')
+        const kwtt = parseInt(input.value) + parseInt(kwt);
+        // kuwantiti.setAttribute('data-qnt', kwtt);
+        // kuwantiti.innerHTML = `Qty: ${kwtt}`;
 
-          const ptotal = modal.querySelector(`#${uid}`).querySelector('#total');
-          const dtotal = ptotal.getAttribute('data-totall');
-          console.log(parseInt(dtotal));
-          const ddtotal = parseInt(dtotal) + parseInt(totalPrice);
-          console.log(ddtotal);
-          ptotal.setAttribute('data-totall', ddtotal);
-          ptotal.innerHTML = `Total Price: ${ddtotal}`
+        // updating the total price of one item.
+        const ptotal = modal.querySelector(`#${uid}`).querySelector('#total');
+        const dtotal = ptotal.getAttribute('data-totall');
+        const ddtotal = parseInt(dtotal) + parseInt(totalPrice);
+        // ptotal.setAttribute('data-totall', ddtotal);
+        // ptotal.innerHTML = `Total Price: ${ddtotal}`
+        console.log('nadagdagan ', totalOfAll, typeof totalOfAll);
+        modal.querySelector(`#${uid}`).remove()
+        modal.innerHTML += `
+        <div id="${uid}" class="col-12 pl-2 pr-4 mb-3">
 
-        } else {
-          modal.innerHTML += `
+        <img class="mr-3" src=${foodImage} style="width: 50px; height: 50px; float:left; border-radius:500%;">
+        <button id="removeTrashBtn" class="fa-solid fa-trash" style="border:none; background-color:white; float:right"></button>
+        <h4 style="font-size:medium;">${foodName}</h4>
+        <h6 id="kuwantiti" style="float:left;" data-qnt="${kwtt}">Qty: ${kwtt}</h6>
+        <p style="float:right; margin-bottom:0rem; padding-top:0rem;">Price: ${value}</p><br>
+        <p id="total" style="float:right; margin-bottom:0rem; padding-top:0rem;" data-totall="${ddtotal}">Total Price: ${ddtotal}</p><br>
+      
+        <br>
+        
+        </div>
+        `
+      } else {
+        // adding a new element in sidebar when the order is dont have a copy in sidebar.
+        modal.innerHTML += `
         <div id="${uid}" class="col-12 pl-2 pr-4 mb-3">
 
         <img class="mr-3" src=${foodImage} style="width: 50px; height: 50px; float:left; border-radius:500%;">
@@ -122,85 +137,92 @@ let totalOfAll = 0;
         
         </div>
         `;
-        }
-
         console.log('nadagdagan ', totalOfAll, typeof totalOfAll);
-
-        // Get the side navigation menu's total element
-        const sideNavTotal = document.querySelector('#mySidenav').childNodes[7].childNodes[1];
-        sideNavTotal.innerHTML = `Total:  ₱${totalOfAll}`;
-
-        const anotherDiv = document.querySelector('#checkOutTotal');
-        anotherDiv.innerHTML = `₱${totalOfAll}`;
-
-        const totalWith30 = totalOfAll + 30;
-        const totalDiv = document.querySelector('#totalDiv');
-        totalDiv.innerHTML = `₱${totalWith30}`;
-
-        modal.querySelector('#total').setAttribute('data-totall', totalPrice)
-
-        input.value = 0;
-
-        modal.style.display = "block";
-
-        // Get a list of all the trash button elements
-        const trashButtons = modal.querySelectorAll('.fa-trash');
-        trashButtons.forEach(function (trashButton) {
-          trashButton.addEventListener('click', function () {
-            const parent = trashButton.parentNode;
-
-            // updating the total in sidebar when user delete an order.
-            const totalOneItem = parent.querySelector('#total').getAttribute('data-totall');
-            const totalOneItemInt = parseInt(totalOneItem);
-
-            totalOfAll = parseInt(totalOfAll) - totalOneItemInt;
-            console.log('totalOfAll: ', totalOfAll, typeof totalOfAll);
-            sideNavTotal.innerHTML = `Total:  ${totalOfAll}`;
-            const anotherDiv = document.querySelector('#checkOutTotal');
-            anotherDiv.innerHTML = `₱${totalOfAll}`;
-
-            const totalWith30 = totalOfAll + 30;
-            const totalDiv = document.querySelector('#totalDiv');
-            totalDiv.innerHTML = `₱${totalWith30}`;
-            parent.remove();
-
-          });
-        });
-
-
+        console.log(" ")
       }
-    } else if (event.target.classList.contains('closeCart')) {
-      document.getElementById("mySidenav").style.width = "0";
+
+
+
+      // Get the side navigation menu's total element
+      const sideNavTotal = document.querySelector('#mySidenav').childNodes[7].childNodes[1];
+      sideNavTotal.innerHTML = `Total:  ₱${totalOfAll}`;
+
+      // const anotherDiv = document.querySelector('#checkOutTotal');
+      // anotherDiv.innerHTML = `₱${totalOfAll}`;
+
+      // const totalWith30 = totalOfAll + 30;
+      // const totalDiv = document.querySelector('#totalDiv');
+      // totalDiv.innerHTML = `₱${totalWith30}`;
+
+     
+
+      input.value = 0;
+
+      modal.style.display = "block";
+
+      // Get a list of all the trash button elements
+      const trashButtons = modal.querySelectorAll('.fa-trash');
+      trashButtons.forEach(function (trashButton) {
+        trashButton.addEventListener('click', function () {
+          console.log('deleted')
+          const parent = trashButton.parentNode;
+          // updating the total in sidebar when user delete an order.
+          const totalOneItem = parent.querySelector('#total').getAttribute('data-totall');
+          console.log(totalOneItem)
+          const totalOneItemInt = parseInt(totalOneItem);
+          console.log('total Of All: ', totalOfAll, typeof totalOfAll);
+          console.log('total of one item '+ totalOneItemInt)
+          totalOfAll = parseInt(totalOfAll) - totalOneItemInt;
+          console.log('total Of All: ', totalOfAll, typeof totalOfAll);
+          console.log('')
+          sideNavTotal.innerHTML = `Total:  ${totalOfAll}`;
+
+          // --------Code ni ms rica para sa check out----------
+          // const anotherDiv = document.querySelector('#checkOutTotal');
+          // anotherDiv.innerHTML = `₱${totalOfAll}`;
+
+          // const totalWith30 = totalOfAll + 30;
+          // const totalDiv = document.querySelector('#totalDiv');
+          // totalDiv.innerHTML = `₱${totalWith30}`;
+          parent.remove();
+
+        });
+      });
+
+
     }
+  } else if (event.target.classList.contains('closeCart')) {
+    document.getElementById("mySidenav").style.width = "0";
   }
-  );
+}
+);
 
-  // to copy the child of #modal into user_Checkout
-  const checkOut = document.querySelector('#checkOut')
-  checkOut.addEventListener('click', () => {
+// to copy the child of #modal into user_Checkout
+const checkOut = document.querySelector('#checkOut')
+checkOut.addEventListener('click', () => {
 
-    const modal = document.querySelector('#modal');
-    const user_Checkout = document.querySelector('#user_Checkout')
-    const children = modal.childNodes;
-    console.log(children)
-    for (let i = 0; i < children.length; i++) {
-      user_Checkout.appendChild(children[i].cloneNode(true));
+  const modal = document.querySelector('#modal');
+  const user_Checkout = document.querySelector('#user_Checkout')
+  const children = modal.childNodes;
+  console.log(children)
+  for (let i = 0; i < children.length; i++) {
+    user_Checkout.appendChild(children[i].cloneNode(true));
 
-    }
+  }
 
-    const trash = user_Checkout.querySelectorAll('#removeTrashBtn')
-    trash.forEach(trash => {
-      trash.remove()
-    })
+  const trash = user_Checkout.querySelectorAll('#removeTrashBtn')
+  trash.forEach(trash => {
+    trash.remove()
   })
+})
 
-  // deleting the child of user_Checkout so there is no double copy.
-  document.querySelector('#btnCloseBtn').addEventListener('click', () => {
-    const user_Checkout = document.querySelector('#user_Checkout')
-    while (user_Checkout.firstChild) {
-      user_Checkout.removeChild(user_Checkout.firstChild);
-    }
+// deleting the child of user_Checkout so there is no double copy.
+document.querySelector('#btnCloseBtn').addEventListener('click', () => {
+  const user_Checkout = document.querySelector('#user_Checkout')
+  while (user_Checkout.firstChild) {
+    user_Checkout.removeChild(user_Checkout.firstChild);
+  }
 
-  })
+})
 
 
